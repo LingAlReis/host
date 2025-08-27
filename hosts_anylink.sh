3e6101c396806f7f9ac396d5997dbcfbf337e467
admin <admin@5210125.xyz>
2025-08-28 00:36:53 +0800

    提交时间：20250828 00 36 提交内容：更新anylink地址

--- hosts_anylink.sh
+++ hosts_anylink.sh
@@ -1,5 +1,8 @@
-# SakuraFrpc
-122.246.0.254    anylink.5210125.xyz anylink
+# anylink
+10.0.0.212       anylink.5210125.xyz anylink
 
-# LosangelesFrpc
+# sakurafrpc
+# 122.246.0.254  anylink.5210125.xyz anylink
+
+# losangelesfrpc
 # 143.20.161.101 anylink.5210125.xyz anylink
