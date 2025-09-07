5b4a79a1b6f593404bae9abf23a4141745b8dd32
admin <admin@5210125.xyz>
2025-09-07 14:07:40 +0800

    提交时间：20250907 14 07 提交内容：修改hosts

--- hosts.sh
+++ hosts.sh
@@ -2,20 +2,24 @@
 127.0.0.1   localhost.localdomain   localhost
 
 10.0.0.1    route.5210125.xyz       route
+
 10.0.0.200  proxmoxve.5210125.xyz   proxmoxve
 10.0.0.201  istoreos.5210125.xyz    istoreos
 10.0.0.202  istoreosdns.5210125.xyz istoreosdns
 10.0.0.203  synology.5210125.xyz    synology
 10.0.0.204  fnos.5210125.xyz        fnos
-10.0.0.210  database.5210125.xyz    database
-10.0.0.211  storage.5210125.xyz     storage
-10.0.0.212  firewall.5210125.xyz    firewall
-10.0.0.213  firewalldns.5210125.xyz firewalldns
-10.0.0.214  monitor.5210125.xyz     monitor
-10.0.0.215  office.5210125.xyz      office
-10.0.0.216  develop.5210125.xyz     develop
-10.0.0.217  media.5210125.xyz       media
+
+10.0.0.211  database.5210125.xyz    database
+10.0.0.212  storage.5210125.xyz     storage
+10.0.0.213  firewall.5210125.xyz    firewall
+10.0.0.214  firewalldns.5210125.xyz firewalldns
+10.0.0.215  monitor.5210125.xyz     monitor
+10.0.0.216  office.5210125.xyz      office
+10.0.0.217  develop.5210125.xyz     develop
+10.0.0.218  media.5210125.xyz       media
+
 10.0.0.241  debian.5210125.xyz      debian
+
 10.0.0.250  onekvm.5210125.xyz      onekvm
 10.0.0.251  macos.5210125.xyz       macos
 10.0.0.252  windows.5210125.xyz     windows
