10a34a2d5e1781f425dc13760807d9ddf52196d9
admin <admin@5210125.xyz>
2025-12-25 02:09:36 +0800

    提交时间：20251225 02 09 提交内容：更新hosts

--- hosts.sh
+++ hosts.sh
@@ -8,12 +8,9 @@
 
 # vip            10.0.0.10X - 10.0.0.19X
 # #######################################################################
-10.0.0.101       anylink.vip.5210125.xyz            anylink.vip
-10.0.0.102       wireguard.vip.5210125.xyz          wireguard.vip
-10.0.0.103       easytier.vip.5210125.xyz           easytier.vip
-10.0.0.104       nginxproxymanager.vip.5210125.xyz  nginxproxymanager.vip
-10.0.0.105       sunpanel.vip.5210125.xyz           sunpanel.vip
 10.0.0.201       adguardhome.vip.5210125.xyz        adguardhome.vip
+10.0.0.101       easytier.vip.5210125.xyz           easytier.vip
+10.0.0.102       wireguard.vip.5210125.xyz          wireguard.vip
 
 # proxmoxve      10.0.0.20X - 10.0.0.22X
 # #######################################################################
@@ -22,23 +19,17 @@
 10.0.0.202       istoreosdns.5210125.xyz            istoreosdns
 10.0.0.203       synology.5210125.xyz               synology
 10.0.0.204       fnos.5210125.xyz                   fnos
+10.0.0.205       windows.5210125.xyz                windows
 10.0.0.211       database.5210125.xyz               database
-10.0.0.212       storage.5210125.xyz                storage
-10.0.0.213       firewall.5210125.xyz               firewall
-10.0.0.214       firewalldns.5210125.xyz            firewalldns
-10.0.0.215       monitor.5210125.xyz                monitor
-10.0.0.216       office.5210125.xyz                 office
-10.0.0.217       develop.5210125.xyz                develop
-10.0.0.218       media.5210125.xyz                  media
-10.0.0.219       debian.5210125.xyz                 debian
-
-# proxmoxveslave 10.0.0.23X - 10.0.0.24X
-# #######################################################################
-10.0.0.230       proxmoxveslave.5210125.xyz         proxmoxveslave
 
 # other          10.0.0.25X - 10.0.0.25X
 # #######################################################################
 10.0.0.250       onekvm.5210125.xyz                 onekvm
+
+# vps
+# #######################################################################
+143.20.161.101   losangeles.5210125.xyz             losangeles
+107.150.19.187   seattle.5210125.xyz                seattle
 
 # The following lines are desirable for IPv6 capable hosts
 # #######################################################################
