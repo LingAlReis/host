ba62983d40c3d0a57ffd9346aacc8c6fbb601143
admin <admin@5210125.xyz>
2026-02-28 11:37:35 +0800

    提交时间：20260228 11 37 提交内容：更新hosts

--- hosts.sh
+++ hosts.sh
@@ -20,7 +20,9 @@
 10.0.0.203       synology.5210125.xyz               synology
 10.0.0.204       fnos.5210125.xyz                   fnos
 10.0.0.205       windows.5210125.xyz                windows
+10.0.0.206       macos.5210125.xyz                  macos
 10.0.0.211       database.5210125.xyz               database
+10.0.0.215       windowslave.5210125.xyz            windowslave
 
 # other          10.0.0.25X - 10.0.0.25X
 # #######################################################################
